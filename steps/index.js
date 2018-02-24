14196ddfe7e45bdb535a3ff9772eece03d1cbbd9
asher978 <ashershaheen@gmail.com>
2018-02-24 17:02:10 -0500

    added solution # 1

---
+++ steps/index.js
@@ -0,0 +1,39 @@
+// Write a function called `steps(n)` that accepts a positive number N
+// as an argueent. The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+
+// SOULTION # 1
+function steps (n) {
+
+  for (let row = 0; row < n; row ++) {
+    let stairs = '';
+
+    for (let col = 0; col < n; col ++) {
+
+      if (col <= row) {
+        stairs += '#';
+      } else {
+        stairs += ' ';
+      }
+    }
+    console.log(stairs)
+  }
+};
+
+
+module.exports = steps;
