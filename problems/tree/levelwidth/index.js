4603af63b3f3a8b53820eb374e6ab42e52d7eae3
asher978 <ashershaheen@gmail.com>
2018-02-28 11:23:00 -0500

    added solution for level width

---
+++ problems/tree/levelwidth/index.js
@@ -0,0 +1,35 @@
+// --- Directions
+// Given the root node of a tree, return
+// an array where each element is the width
+// of the tree at each level.
+// --- Example
+// Given:
+//     0
+//   / |  \
+// 1   2   3
+// |       |
+// 4       5
+// Answer: [1, 3, 2]
+
+
+// usage of traverseBF on a tree
+function levelWidth(root) {
+  const arr = [root, 'N'];
+  
+  let counter = [0];
+
+  while(arr.length > 1) {
+    const node = arr.shift();
+    
+    if (node === 'N') {
+      counter.push(0);
+      arr.push('N');
+    } else {
+      arr.push(...node.children);
+      counter[counter.length-1]++;
+    }
+  }
+  return counter;
+};
+
+module.exports = levelWidth;
